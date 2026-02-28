6f500155b77d5acb1934b5eb7e45e1a1396ecf3e
YusufBro01 <ibrohimovyusufbro@gmail.com>
2026-02-28 23:49:40 +0500

    first commit

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Yusufbee — 3D Portfolio",
+    fullName: "Yusufbee",
+    email: "ibrohimovyusufbro@mail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Yusufbee",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
