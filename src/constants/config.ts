961626348ba4e437699a9ab3b6a0e210c51132a6
YusufBro01 <ibrohimovyusufbro@gmail.com>
2026-03-01 00:24:38 +0500

    dwfef

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -84,7 +84,7 @@ export const config: TConfig = {
     works: {
       p: "My work",
       h2: "Projects.",
-      content: `Following projects showcases my skills and experience through
+      content: `Folloving projects showcases my skills and experience through
     real-world examples of my work. Each project is briefly described with
     links to code repositories and live demos in it. It reflects my
     ability to solve complex problems, work with different technologies,
