940727743889307954bd6138604c670c0561379b
YusufBro01 <ibrohimovyusufbro@gmail.com>
2026-03-01 12:19:17 +0500

    dnfnnsndn

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -147,7 +147,7 @@ const experiences: TExperience[] = [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Participating in code reviews and providing constructive feedback to other developers. 0709",
     ],
   },
   {
